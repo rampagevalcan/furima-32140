47ee0fe32f0de524325aa1880283da05dddf6e1e
rampagevalcan <takeofftowardadream1019@gmail.com>
2020-12-07 17:05:07 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,15 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+validates :nickname, presence: true
+validates :email, uniqueness: true, presence: true
+validates :birthday, presence: true
+with_options presence: true do
+  validates :encrypted_password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i }
+  validates :family_name, format: { with:/\A[ぁ-んァ-ン一-龥]/ }
+  validates :first_name, format: { with:/\A[ぁ-んァ-ン一-龥]/ }
+  validates :family_name_kana, format: { with:/\A[ァ-ヶー－]+\z/ }
+  validates :first_name_kana, format: { with:/\A[ァ-ヶー－]+\z/ }
+end
 end
