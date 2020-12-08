a7a9b52c0442ef54f1a8419bbffe704ffaf06297
rampagevalcan <takeofftowardadream1019@gmail.com>
2020-12-08 20:16:13 +0900

    Rubocop実行

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,13 +4,13 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-validates :nickname, presence: true
-validates :birthday, presence: true
-with_options presence: true do
-  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i }
-  validates :family_name, format: { with:/\A[ぁ-んァ-ン一-龥]+\z/ }
-  validates :first_name, format: { with:/\A[ぁ-んァ-ン一-龥]+\z/ }
-  validates :family_name_kana, format: { with:/\A[ァ-ヶー－]+\z/ }
-  validates :first_name_kana, format: { with:/\A[ァ-ヶー－]+\z/ }
-end
+  validates :nickname, presence: true
+  validates :birthday, presence: true
+  with_options presence: true do
+    validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i }
+    validates :family_name, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ }
+    validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ }
+    validates :family_name_kana, format: { with: /\A[ァ-ヶー－]+\z/ }
+    validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/ }
+  end
 end
