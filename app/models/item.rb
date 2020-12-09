e4b62849450aee5c94a4cb3651bd8eb44ebc1b3e
rampagevalcan <takeofftowardadream1019@gmail.com>
2020-12-09 18:46:46 +0900

    ActiveHashの導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,10 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :category
+  belongs_to_active_hash :item_condition
+  belongs_to_active_hash :shopping_fee
+  belongs_to_active_hash :prefecture
+  belongs_to_active_hash :delivery_time
   belongs_to :user
   has_one_attached :image
 end
