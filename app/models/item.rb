9b054500456c1fc79363b1494e97a879a951c0a0
rampagevalcan <takeofftowardadream1019@gmail.com>
2020-12-10 10:55:16 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,4 +7,19 @@ class Item < ApplicationRecord
   belongs_to_active_hash :delivery_time
   belongs_to :user
   has_one_attached :image
+
+  validates :image, presence: true
+  validates :item_name, presence: true
+  validates :item_description, presence: true
+with_options presence: true do
+  with_options numericality:{ other_than: 1 } do
+  validates :category_id
+  validates :item_condition_id
+  validates :shopping_fee_id
+  validates :prefecture_id
+  validates :delivery_time_id
+  end
+  validates :price, numericality:{ greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
+  validates :price, numericality:{ only_integer:true }
+end
 end
