5f36f59fb066959f7193f0716bfa093aa252ea8f
rampagevalcan <takeofftowardadream1019@gmail.com>
2020-12-10 18:17:35 +0900

    商品出品機能テストコードの実行

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,83 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+before do
+  @item = FactoryBot.build(:item)
+end
+
+describe '商品出品登録' do
+  context '商品出品登録がうまくいくとき' do
+    it '全ての値が正しく入力されていれば登録される' do
+      expect(@item).to be_valid
+    end
+  end
+
+  context '商品登録がうまくいかないとき' do
+    it '商品画像がないとき' do
+      @item.image = nil
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Image can't be blank")
+    end
+
+    it '商品の説明がないとき' do
+      @item.item_name = nil
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Item name can't be blank")
+    end
+
+    it 'カテゴリーの情報がないとき' do
+      @item.category_id = 1
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Category must be other than 1")
+    end
+
+    it '商品の状態についての情報がないとき' do
+      @item.item_condition_id = 1
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Item condition must be other than 1")
+    end
+
+    it '配送料の負担についての情報がないとき' do
+      @item.shopping_fee_id = 1
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Shopping fee must be other than 1")
+    end
+
+    it '発送元の地域についての情報がないとき' do
+      @item.prefecture_id = 1
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Prefecture must be other than 1")
+    end
+
+    it '発送までの日数についての情報がないとき' do
+      @item.delivery_time_id = 1
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Delivery time must be other than 1")
+    end
+
+    it '価格についての情報がないとき' do
+      @item.price = nil
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Price can't be blank")
+    end
+
+    it '価格が300円未満のとき' do
+      @item.price = 1
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Price must be greater than or equal to 300")
+    end
+
+    it '価格が9999999円を超えるとき' do
+      @item.price = 10000000
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Price must be less than or equal to 9999999")
+    end
+
+    it '販売価格が半角英数で入力されてないとき' do
+      @item.price = '１０００'
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Price is not a number")
+    end
+  end
+end
 end
