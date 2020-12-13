1ff47bd4cb735f86fc0a400067b80c7117715416
rampagevalcan <takeofftowardadream1019@gmail.com>
2020-12-13 17:31:17 +0900

    コントローラーの設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :edit]
-  before_action :set_item, only: [:show, :edit, :update]
+  before_action :set_item, only: [:show, :edit, :update, :destroy]
   def index
     @items = Item.includes(:user).order('created_at DESC')
   end
@@ -30,6 +30,12 @@ class ItemsController < ApplicationController
       redirect_to item_path(@item.id)
     else
       render action: :edit
+    end
+  end
+
+  def destroy
+    if @item.destroy
+      redirect_to root_path
     end
   end
 
