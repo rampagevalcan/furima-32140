42a0a1f1c08e6de0101fe2ba2ca7797f0190df04
rampagevalcan <takeofftowardadream1019@gmail.com>
2020-12-13 12:22:08 +0900

    updateアクションのルーティングの実装

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
 
   get 'items/index'
   root to: "items#index"
-  resources :items, only: [:new ,:create, :index, :show, :edit] do
+  resources :items, only: [:new ,:create, :index, :show, :edit, :update] do
     end
 end
