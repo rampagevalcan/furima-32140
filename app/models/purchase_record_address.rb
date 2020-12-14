8c895ab6ba0711763d42713e3f03add51622cf87
rampagevalcan <takeofftowardadream1019@gmail.com>
2020-12-14 19:30:52 +0900

    データをテーブルに保存する処理の記述

--- app/models/purchase_record_address.rb
+++ app/models/purchase_record_address.rb
@@ -9,4 +9,10 @@ class PurchaseRecordAddress
     validates :telephone_number,format: {with: /\A\d{10}\z|\A\d{11}\z/}
 end
   validates :prefecture_id, numericality: { other_than: 1,message: "can't be blank"}
+
+def save
+  purchase_record = PurchaseRecord.create(user_id: user_id, item_id: item_id)
+  Address.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, house_number:house_number, building_name: building_name, telephone_number: telephone_number, purchase_record_id:purchase_record_id)
 end
+end
+
