6e4b3f292d579c262b23c9a9b47228f57957b44a
rampagevalcan <takeofftowardadream1019@gmail.com>
2020-12-17 21:09:20 +0900

    rubocop 実行

--- app/controllers/purchase_records_controller.rb
+++ app/controllers/purchase_records_controller.rb
@@ -21,22 +21,20 @@ class PurchaseRecordsController < ApplicationController
   private
 
   def purchase_record_params
-    params.require(:purchase_record_address).permit(:postal_code, :prefecture_id, :city, :house_number, :building_name, :telephone_number).merge(user_id:current_user.id,token: params[:token], item_id:params[:item_id] )
+    params.require(:purchase_record_address).permit(:postal_code, :prefecture_id, :city, :house_number, :building_name, :telephone_number).merge(user_id: current_user.id, token: params[:token], item_id: params[:item_id])
   end
 
   def pay_item
-      Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
-      Payjp::Charge.create(
-        amount: @item.price,
-        card: purchase_record_params[:token],
-        currency: 'jpy'
-      )
-    end
-
-    def move_to_index
-      @item = Item.find(params[:item_id])
-      unless @item.user_id != current_user.id
-        redirect_to root_path
-      end
-    end
+    Payjp.api_key = ENV['PAYJP_SECRET_KEY']
+    Payjp::Charge.create(
+      amount: @item.price,
+      card: purchase_record_params[:token],
+      currency: 'jpy'
+    )
   end
+
+  def move_to_index
+    @item = Item.find(params[:item_id])
+    redirect_to root_path unless @item.user_id != current_user.id
+  end
+end
--- app/models/purchase_record_address.rb
+++ app/models/purchase_record_address.rb
@@ -4,16 +4,15 @@ class PurchaseRecordAddress
 
   with_options presence: true do
     validates :token
-    validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
-    validates :city, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "is invalid. Input full-width characters."}
+    validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'is invalid. Include hyphen(-)' }
+    validates :city, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: 'is invalid. Input full-width characters.' }
     validates :house_number
-    validates :telephone_number,format: {with: /\A\d{10}\z|\A\d{11}\z/}
-end
-  validates :prefecture_id, numericality: { other_than: 1,message: "can't be blank"}
+    validates :telephone_number, format: { with: /\A\d{10}\z|\A\d{11}\z/ }
+  end
+  validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
 
-def save
-  purchase_record = PurchaseRecord.create(user_id: user_id, item_id: item_id)
-  Address.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, house_number:house_number, building_name: building_name, telephone_number: telephone_number, purchase_record_id: purchase_record.id)
+  def save
+    purchase_record = PurchaseRecord.create(user_id: user_id, item_id: item_id)
+    Address.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, house_number: house_number, building_name: building_name, telephone_number: telephone_number, purchase_record_id: purchase_record.id)
+  end
 end
-end
-
